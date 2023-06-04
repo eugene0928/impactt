fae76f784c64733a1a5f41ac9dfdc68c20231ff8
eugene <umid09tuxtayev@gmail.com>
2023-06-05 00:21:40 +0500

    fix: typeorm decorator

---
+++ src/decorators/typeorm-ex.decorator.ts
@@ -0,0 +1,10 @@
+import { SetMetadata } from '@nestjs/common';
+
+export const TYPEORM_EX_CUSTOM_REPOSITORY = 'TYPEORM_EX_CUSTOM_REPOSITORY';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/ban-types
+export function CustomRepository(entity: Function): ClassDecorator {
+
+    return SetMetadata(TYPEORM_EX_CUSTOM_REPOSITORY, entity);
+
+}
