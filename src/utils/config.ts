c041d4f8ac8fe5b2b77266de0c9043c67ee8f9f4
eugene <umid09tuxtayev@gmail.com>
2023-06-04 23:18:10 +0500

    fix(conf): confs

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,38 +4,44 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DataSourceOptions } from 'typeorm';
 
 export const configuration = {
-  isProd: process.env['APP_STATUS'] == 'prod',
-  port: process.env['PORT'],
-  getDataSourceConfig(): DataSourceOptions {
-    return {
-      type: 'postgres' as any,
-      host: process.env['DB_HOST'],
-      port: parseInt(process.env['DB_PORT']),
-      username: process.env['DB_USERNAME'],
-      password: process.env['DB_PASSWORD'],
-      database: process.env['DB_DATABASE'],
-      entities: [join(__dirname, `../**/entities/**.entity.{ts,js}`)],
-      synchronize: true,
-    };
-  },
-  getTypeOrmConfig(): TypeOrmModuleOptions {
-    const ormConfig = {
-      type: 'postgres' as any,
-      host: process.env['DB_HOST'],
-      port: parseInt(process.env['DB_PORT']),
-      username: process.env['DB_USERNAME'],
-      password: process.env['DB_PASSWORD'],
-      database: process.env['DB_DATABASE'],
-      entities: [join(__dirname, `../**/entities/**.entity.{ts,js}`)],
-      synchronize: true,
-      logging: false,
-    };
+    isProd: process.env['APP_STATUS'] == 'prod', 
+    port: process.env['PORT'],
+    getDataSourceConfig(): DataSourceOptions {
 
-    // WARNING!!! Don't change to TRUE in PRODUCTION
-    // if TRUE auto changed DB by Entity model
-    if (configuration.isProd) {
-      ormConfig.synchronize = false;
+        return {
+            type: process.env['DB_TYPE'] as any,
+            host: process.env['DB_HOST'],
+            port: parseInt(process.env['DB_PORT']),
+            username: process.env['DB_USERNAME'],
+            password: process.env['DB_PASSWORD'],
+            database: process.env['DB_DATABASE'],
+            entities: [join(__dirname, `../**/entities/**.entity.{ts,js}`)],
+            synchronize: true
+        };
+    
+    },
+    getTypeOrmConfig(): TypeOrmModuleOptions {
+
+        const ormConfig = {
+            type: process.env['DB_TYPE'] as any,
+            host: process.env['DB_HOST'],
+            port: parseInt(process.env['DB_PORT']),
+            username: process.env['DB_USERNAME'],
+            password: process.env['DB_PASSWORD'],
+            database: process.env['DB_DATABASE'],
+            entities: [join(__dirname, `../**/entities/**.entity.{ts,js}`)],
+            synchronize: true,
+            logging: false
+        };
+
+        // WARNING!!! Don't change to TRUE in PRODUCTION
+        // if TRUE auto changed DB by Entity model
+        if (configuration.isProd) {
+
+            ormConfig.synchronize = false;
+        
+        }
+        return ormConfig;
+    
     }
-    return ormConfig;
-  },
 };
