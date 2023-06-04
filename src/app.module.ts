852f902459ba69f538d0aba0813364d20acdec88
eugene <umid09tuxtayev@gmail.com>
2023-06-04 23:19:36 +0500

    fix(main): seed module and orm integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+// import { SeedModule } from './seed/seed.module';
+// import { configuration } from '@utils/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import * as process from 'process';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { configuration } from "@utils/config";
+import { SeedModule } from "./seed/seed.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: join(process.cwd(), '.env')
+        }),
+        TypeOrmModule.forRoot(configuration.getTypeOrmConfig()),
+        SeedModule
+    ],
+    controllers: [],
+    providers: []
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { configuration } from '@utils/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as process from "process";
 
-const logger = new Logger('Main');
+const logger = new Logger('Main'); 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(configuration.port);
+
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true,
+            transform: true
+        })
+    );
+
+    await app.listen(process.env.PORT);
+
 }
 bootstrap()
-  .then(() => {
-    logger.log(`Server is running on port: [${configuration.port}]`);
-  })
-  .catch((err) => {
-    logger.log(`Error is occurred during initialization the server: [${err}]`);
-  });
+    .then(() => {
+
+        logger.log(`Server is running on port: [${process.env.PORT}]`);
+    
+    })
+    .catch((err) => {
+
+        logger.log(`Error is occurred during initialization the server: [${err}]`);
+    
+    });
