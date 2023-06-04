391814a89cf45ee1932c42bcf773044e02383091
eugene <umid09tuxtayev@gmail.com>
2023-06-04 18:50:32 +0500

    fix: on start up

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configuration } from '@utils/config';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('Main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(configuration.port);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    logger.log(`Server is running on port: [${configuration.port}]`);
+  })
+  .catch((err) => {
+    logger.log(`Error is occurred during initialization the server: [${err}]`);
+  });
