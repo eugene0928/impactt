89c7dd4f104f8452c6a371db58371f9bd4e9d030
eugene <umid09tuxtayev@gmail.com>
2023-06-05 17:23:53 +0500

    test: auth module

---
+++ test/auth/auth.e2e.ts
@@ -0,0 +1,64 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from "@nestjs/common";
+import * as request from 'supertest';
+import { AppModule } from "../../src/app.module";
+import { LoginDto } from "../../src/auth/dto/login.dto";
+
+export const authTests = () => {
+
+    describe('AuthModule (e2e)', () => {
+
+        let app: INestApplication;
+
+        beforeEach(async () => {
+
+            const moduleFixture: TestingModule = await Test.createTestingModule({
+                imports: [AppModule]
+            }).compile();
+
+            app = moduleFixture.createNestApplication();
+            await app.init();
+
+        });
+
+        afterAll(async () => {
+
+            await app.close();
+        
+        });
+
+        it('/login --> 200(POST)', () => {
+
+            const admin: LoginDto = { name: 'SuperAdmin' };
+
+            return request(app.getHttpServer())
+                .post('/login')
+                .send(admin)
+                .expect(200)
+                .expect(({ body }) => {
+
+                    expect(body).toEqual({ status: HttpStatus.OK, msg: expect.any(String), data: expect.any(String) });
+                
+                });
+
+        });
+
+        it('/login --> 404(POST)', async () => {
+
+            const admin: LoginDto = { name: 'Admin' };
+
+            return request(app.getHttpServer())
+                .post('/login')
+                .send(admin)
+                .expect(404)
+                .expect(({ body }) => {
+
+                    expect(body).toEqual({ status: HttpStatus.NOT_FOUND, msg: 'User is not found', data: null });
+
+                });
+
+        });
+
+    });
+
+};
