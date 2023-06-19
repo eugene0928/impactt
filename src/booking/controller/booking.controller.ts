0508b41a3bccfb4cf2db5cf3a09c40a73c51e214
eugene <umid09tuxtayev@gmail.com>
2023-06-19 10:16:59 +0500

    feat(book): free slots

--- src/booking/controller/booking.controller.ts
+++ src/booking/controller/booking.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, HttpStatus, Param, ParseIntPipe, Post, Res } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    DefaultValuePipe,
+    Get,
+    HttpStatus,
+    Param,
+    ParseIntPipe,
+    Post,
+    Query,
+    Res
+} from "@nestjs/common";
 import { BookingService } from "../service/booking.service";
 import { BookingDto } from "../dto/booking.dto";
 import { Response } from "express";
@@ -21,6 +32,22 @@ export class BookingController {
         }
 
         res.status(response.status).json({ 'error': response.message });
+    
+    }
+
+    @Get('/rooms/:id/availability')
+    async roomAvailability(@Param('id', ParseIntPipe) id: number, @Res() res: Response, @Query('date', new DefaultValuePipe(new Date().toISOString())) date: string) {
+
+        const response = await this.bookingService.roomAvailability(id, new Date(date));
+        if(response.status === HttpStatus.OK) {
+
+            res.status(response.status).json(response.data);
+        
+        } else {
+
+            res.status(response.status).json({ 'error': response.message });
+        
+        }
     
     }
 
