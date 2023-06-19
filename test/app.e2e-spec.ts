9ce04602c00760d1f5bca843709f207a2c1ebd43
eugene <umid09tuxtayev@gmail.com>
2023-06-19 11:31:19 +0500

    test: book

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,7 @@
 import { authTests } from "./auth/auth.e2e";
 import { roomE2e } from "./room/room.e2e";
+import { bookingE2e } from "./booking/booking.e2e";
 
 describe('auth', authTests);
 describe('room', roomE2e);
+describe('booking', bookingE2e);
