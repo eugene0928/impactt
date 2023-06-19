282edfe49a8cc642aa24c20a8b8c5b94157ad727
eugene <umid09tuxtayev@gmail.com>
2023-06-19 12:00:38 +0500

    fix: dataSource options

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -14,7 +14,7 @@ export const configuration = {
             port: parseInt(process.env['DB_PORT']),
             username: process.env['DB_USERNAME'],
             password: process.env['DB_PASSWORD'],
-            database: 'impactt',
+            database: process.env['DB_DATABASE'] || 'impactt',
             entities: [join(__dirname, `../**/entities/**.entity.{ts,js}`)],
             synchronize: true
         };
